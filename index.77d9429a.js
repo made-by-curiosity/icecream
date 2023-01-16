1e19b3e503145e28a9406f14409f788fad0dd3f9
Turt1ePower <93835953+Turt1ePower@users.noreply.github.com>
2023-01-16 14:37:50 +0000

    Deploying to gh-pages from @ Turt1ePower/fs74-icecream-project17@81fd01611a509b476c0a02aca76fb2a92d3dffd7 🚀

---
+++ index.77d9429a.js
@@ -0,0 +1,2 @@
+function backToTop(){var o=this,n=$(".back-to-top");$(window).on("scroll",(function(){$(o).scrollTop()>=50?n.fadeIn():n.fadeOut()})),n.on("click",(function(o){o.preventDefault(),$("html").animate({scrollTop:0},1500)}))}backToTop();
+//# sourceMappingURL=index.77d9429a.js.map
