d9f0781cda582b90d89fe8dc466de24a5374b77d
HannaStrat <hanna.vital19@gmail.com>
2023-01-16 13:51:02 +0200

    add script

---
+++ src/js/gallery.js
@@ -0,0 +1,10 @@
+$('.gallery-slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    speed: 1500,
+    cssEase: 'linear'
+});
